a9b8dbf702bcf1386b19a2776e2576f3f83d5218
Intars Students <the.mobix@gmail.com>
2011-12-23 01:18:04 +0200

    fixed node websocket server

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
-var sys = require("sys"),
-    http = require("http"),
+// npm install git://github.com/Worlize/WebSocket-Node.git
+
+var WebSocketServer = require('websocket').server,
+    http = require('http'),
     lib = require("./lib.js"),
-    ws = require("./node-websocket-server/lib/ws/server"),
     methods = ["play", "stop", "prev", "next", "favorite", "voteup", "votedown", "voteclear", "mute", "volup", "voldown"],
     version = "0.1";
- 
-console.log("nodeSharky v." + version);
 
-
-var httpServer = http.createServer(function(req, res){
+var server = http.createServer(function(req, res) {
   if (req.method == "GET"){
     var toggle = /\/(\w+)/i.exec(req.url);
     
     if (toggle != null && lib.in_array(toggle[1], methods)){
-      server.broadcast(toggle[1]);
+      wsServer.broadcast(toggle[1]);
       res.writeHead(200);
       res.write("TOGGLING (" + toggle[1] + ") OK");
     }else{
@@ -26,16 +24,46 @@ var httpServer = http.createServer(function(req, res){
   res.end();
 });
 
-var server = ws.createServer({
-  server: httpServer
+server.listen(8181, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
 });
 
-server.addListener("listening", function(){
-  console.log("Waiting for requests ...");
+wsServer = new WebSocketServer({
+    httpServer: server,
+    // You should not use autoAcceptConnections for production
+    // applications, as it defeats all standard cross-origin protection
+    // facilities built into the protocol and the browser.  You should
+    // *always* verify the connection's origin and decide whether or not
+    // to accept it.
+    autoAcceptConnections: false
 });
 
-server.addListener("connection", function(conn){
-  console.log("Client connected (" + conn.id + ")")
-});
+function originIsAllowed(origin) {
+  // put logic here to detect whether the specified origin is allowed.
+  return true;
+}
 
-server.listen(8181);
+wsServer.on('request', function(request) {
+    if (!originIsAllowed(request.origin)) {
+      // Make sure we only accept requests from an allowed origin
+      request.reject();
+      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+      return;
+    }
+
+    var connection = request.accept(null, request.origin);
+    console.log((new Date()) + ' Connection accepted.');
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ' + message.utf8Data);
+            connection.sendUTF(message.utf8Data);
+        }
+        else if (message.type === 'binary') {
+            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+            connection.sendBytes(message.binaryData);
+        }
+    });
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
+});
