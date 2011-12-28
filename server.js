454259159c897874b6fee188c7271342d4611268
Intars Students <the.mobix@gmail.com>
2011-12-28 23:59:28 +0200

    some changes to code and server debug output, added optimist argv parser

--- server.js
+++ server.js
@@ -1,69 +1,57 @@
-// npm install git://github.com/Worlize/WebSocket-Node.git
+var argv = require('optimist')
+	.default({p: 8800})
+	.argv
+;
 
 var WebSocketServer = require('websocket').server,
-    http = require('http'),
-    lib = require("./lib.js"),
-    methods = ["play", "stop", "prev", "next", "favorite", "voteup", "votedown", "voteclear", "mute", "volup", "voldown"],
-    version = "0.1";
+		http = require('http'),
+		commons = require("./commons.js"),
+		methods = ["play", "stop", "prev", "next", "favorite", "voteup", "votedown", "voteclear", "mute", "volup", "voldown"],
+		version = "0.1";
 
 var server = http.createServer(function(req, res) {
-  if (req.method == "GET"){
-    var toggle = /\/(\w+)/i.exec(req.url);
-    
-    if (toggle != null && lib.in_array(toggle[1], methods)){
-      wsServer.broadcast(toggle[1]);
-      res.writeHead(200);
-      res.write("TOGGLING (" + toggle[1] + ") OK");
-    }else{
-      res.writeHead(500);
-      res.write("Method doesn't excist!");
-    }
-    
-  }
-  
-  res.end();
+
+	console.log((new Date()) + ' request recieved ' + req.method + ' ' + req.url);
+
+	if (req.method == "GET"){
+		var toggle = /\/(\w+)/i.exec(req.url);
+		
+		if (toggle != null && commons.in_array(toggle[1], methods)){
+
+			socket.broadcast(toggle[1]);
+			console.log((new Date()) + ' brodcasting ' + toggle[1] + ' to peers');
+
+			res.writeHead(200);
+			res.write("TOGGLING (" + toggle[1] + ") OK");
+
+		}else{
+
+			res.writeHead(500);
+			res.write("Method doesn't excist!");
+
+		}
+		
+	}
+	
+	res.end();
+}).listen(argv.p, function() {
+
+		console.log((new Date()) + ' nodeSharky ' + version + ' is listening on port ' + argv.p);
+
 });
 
-server.listen(8181, function() {
-    console.log((new Date()) + ' Server is listening on port 8080');
-});
+var socket = new WebSocketServer({
 
-wsServer = new WebSocketServer({
-    httpServer: server,
-    // You should not use autoAcceptConnections for production
-    // applications, as it defeats all standard cross-origin protection
-    // facilities built into the protocol and the browser.  You should
-    // *always* verify the connection's origin and decide whether or not
-    // to accept it.
-    autoAcceptConnections: false
-});
+	httpServer: server,
+	autoAcceptConnections: false
 
-function originIsAllowed(origin) {
-  // put logic here to detect whether the specified origin is allowed.
-  return true;
-}
+}).on('request', function(request) {
 
-wsServer.on('request', function(request) {
-    if (!originIsAllowed(request.origin)) {
-      // Make sure we only accept requests from an allowed origin
-      request.reject();
-      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-      return;
-    }
+		var connection = request.accept(null, request.origin);
+		console.log((new Date()) + ' peer ' + connection.remoteAddress + ' connected');
 
-    var connection = request.accept(null, request.origin);
-    console.log((new Date()) + ' Connection accepted.');
-    connection.on('message', function(message) {
-        if (message.type === 'utf8') {
-            console.log('Received Message: ' + message.utf8Data);
-            connection.sendUTF(message.utf8Data);
-        }
-        else if (message.type === 'binary') {
-            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-            connection.sendBytes(message.binaryData);
-        }
-    });
-    connection.on('close', function(reasonCode, description) {
-        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-    });
+		connection.on('close', function(reasonCode, description) {
+				console.log((new Date()) + ' peer ' + connection.remoteAddress + ' disconnected.');
+		});
+
 });
